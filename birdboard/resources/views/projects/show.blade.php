5ec0e692a39882a26a6cadc774ff5f469a87aba8
Matt Casanova <matt.c@vshred.com>
2019-08-15 08:38:55 -0700

    Completed Ep 11

--- birdboard/resources/views/projects/show.blade.php
+++ birdboard/resources/views/projects/show.blade.php
@@ -3,30 +3,40 @@
 @section('content')
     <header class="flex items-center mb-3 py-3">
         <div class="flex justify-between w-full items-end">
-            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+
+            <p class="text-grey text-sm font-normal">
+                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+
             <a class="button" href="/projects/create">New Proejct</a>
         </div>
     </header>
 
     <main>
-        <div class="flex">
-            <div>
-                <h2 class="text-grey text-sm font-normal">Tasks</h2>
-                {{-- tasks --}}
+        <div class="lg:flex -mx-3">
 
-                <h2 class="text-grey text-sm font-normal">General Notes</h2>
-                
-                {{-- General Notes --}}
-                <div class="card">Lorem ipsum.</div>
-            </div>
-
-            <div>
-                <div class="card">
-                    <h1>{{ $project->title}}</h1>
-                    <div>{{ $project->description }}</div>
-                    <a href="/projects">Go Back</a>
+            <div class="lg:w-3/4 px-3  mb-6">
+                <div class="mb-8">
+                    {{-- tasks --}}
+                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card">Lorem ipsum.</div>
                 </div>
+
+                <div>
+                    {{-- General Notes --}}
+                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+                    <textarea class="card w-full" style="min-height: 200px">Lorem ipsum.</textarea>
+                </div>
+      
             </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+
         </div>
     </main>
 @endsection
