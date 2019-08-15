9758380eb73990e345cc55b096d61824ca895beb
Matt Casanova <matt.c@vshred.com>
2019-08-14 17:33:24 -0700

    Working

--- birdboard/resources/views/projects/show.blade.php
+++ birdboard/resources/views/projects/show.blade.php
@@ -1,8 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title}}</h1>
-    <div>{{ $project->description }}</div>
+    <header class="flex items-center mb-3 py-3">
+        <div class="flex justify-between w-full items-end">
+            <h2 class="text-grey text-sm font-normal">My Projects</h2>
+            <a class="button" href="/projects/create">New Proejct</a>
+        </div>
+    </header>
 
-    <a href="/projects">Go Back</a>
+    <main>
+        <div class="flex">
+            <div>
+                <h2 class="text-grey text-sm font-normal">Tasks</h2>
+                {{-- tasks --}}
+
+                <h2 class="text-grey text-sm font-normal">General Notes</h2>
+                
+                {{-- General Notes --}}
+                <div class="card">Lorem ipsum.</div>
+            </div>
+
+            <div>
+                <div class="card">
+                    <h1>{{ $project->title}}</h1>
+                    <div>{{ $project->description }}</div>
+                    <a href="/projects">Go Back</a>
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
